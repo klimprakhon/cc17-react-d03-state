1c45c4505e90d25a94b525ac0a35c46373a66e21
klimprakhon <klimprakhon@gmail.com>
2024-04-30 10:34:34 +0700

    finish ui

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-// import React from "react";
+// Import Third party
+import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css"; // Global CSS
 
 function App() {
-  return <div>Hello React</div>;
+  return(
+    <div className="app">
+      <form className="form">
+        <h2>Register form</h2>
+        <input />
+        <input />
+        <input />
+        <button>Submit</button>
+      </form>
+    </div>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
